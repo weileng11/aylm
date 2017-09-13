29ac070eeb8794465279a5d63e33f48a07f19442
weileng143@163.com <275762645@qq.com>
2017-09-13 10:03:52 +0800

    mvp继续

---
+++ app/src/main/java/cn/wl/aylm/adapter/test.java
@@ -0,0 +1,13 @@
+package cn.wl.aylm.adapter;
+
+/**
+ * Author:Bruce
+ * Package:cn.wl.aylm.adapter
+ * time:2017/9/13.
+ * contact：weileng143@163.com
+ *
+ * @description
+ */
+
+public class test {
+}
---
+++ app/src/main/java/cn/wl/aylm/fragment/test.java
@@ -0,0 +1,13 @@
+package cn.wl.aylm.fragment;
+
+/**
+ * Author:Bruce
+ * Package:cn.wl.aylm.fragment
+ * time:2017/9/13.
+ * contact：weileng143@163.com
+ *
+ * @description
+ */
+
+public class test {
+}
